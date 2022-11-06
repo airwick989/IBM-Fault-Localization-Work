d7cd027ac08f24bf293435250e95943d3d35d7ce
airwick989 <73313597+airwick989@users.noreply.github.com>
2022-11-06 16:50:23 -0500

    installed daisyui plugin for tailwind css and upgraded react scripts

--- Initial Build/client/src/App.js
+++ Initial Build/client/src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import './App.css'
 
 function App(){
 
@@ -23,9 +24,15 @@ function App(){
 
     <div>
 
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content text-center">
+          <div class="max-w-md">
+            <h1 class="text-5xl font-bold">Hello there</h1>
+            <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <button class="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
 
       {(typeof data.members == 'undefined') ? (
         <p>Loading ...</p>
