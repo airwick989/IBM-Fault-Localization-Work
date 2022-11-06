6afcf5201f31eb662c7c46dc2ea20ba1859b9f41
airwick989 <73313597+airwick989@users.noreply.github.com>
2022-11-06 15:14:10 -0500

    Initialized sample backend api data

---
+++ Initial Build/flask-server/server.py
@@ -0,0 +1,14 @@
+from pickle import TRUE
+from flask import Flask
+import json
+
+app = Flask(__name__)
+
+# Members API Route
+@app.route("/members")
+def members():
+    members = {"Member 1", "Member 2", "Member 3"}
+    return json.dumps(list(members))
+
+if __name__ == "__main__":
+    app.run(debug=True)
