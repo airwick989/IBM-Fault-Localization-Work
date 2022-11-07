d19f45c894dd8853a5435b37a45d61edf26251bb
airwick989 <73313597+airwick989@users.noreply.github.com>
2022-11-07 18:54:13 -0500

    Implemented form for uploading required files

--- Initial Build/client/src/Home.js
+++ Initial Build/client/src/Home.js
@@ -1,18 +1,14 @@
 // eslint-disable-next-line
 import { Link } from 'react-router-dom';
+// eslint-disable-next-line
 import React, { useState } from "react";
+import { FileUploader } from './components/FileUploader';
 
 const Home = () => {
 
-    const [fileJLM, setFileJLM] = useState("");
-    const [filePerf, setFilePerf] = useState("");
-    const [fileTest, setFileTest] = useState("");
-
-
+    // eslint-disable-next-line
     const handleSubmit = (e) => {
         e.preventDefault();
-        const dataSent = { fileJLM, filePerf, fileTest };
-        console.log(dataSent)
     }
 
 
@@ -22,53 +18,7 @@ const Home = () => {
             <div class="hero min-h-screen bg-base-200">
             <div class="hero-content text-center">
 
-                <form onSubmit={handleSubmit}>
-
-                    <div class="max-w-md">
-                        <h1 class="text-5xl font-bold">Enter Your Files Here</h1>
-                        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        
-                        <label class="label">
-                            <span class="label-text">JLM Data CSV</span>
-                        </label>
-                        <input type="file" 
-                            class="file-input file-input-bordered file-input-accent w-full max-w-xs" 
-                            style={{marginBottom: 50}}
-                            value={fileJLM}
-                            name="fileJLM"
-                            required
-                            onChange={e => setFileJLM(e.target.value)}
-                        />
-
-                        <label class="label">
-                            <span class="label-text">Perf Data CSV</span>
-                        </label>
-                        <input type="file" 
-                            class="file-input file-input-bordered file-input-accent w-full max-w-xs" 
-                            style={{marginBottom: 50}}
-                            value={filePerf}
-                            name="filePerf"
-                            required
-                            onChange={e => setFilePerf(e.target.value)}
-                        />
-
-                        <label class="label">
-                            <span class="label-text">Test Data CSV</span>
-                        </label>
-                        <input type="file" 
-                            class="file-input file-input-bordered file-input-accent w-full max-w-xs" 
-                            style={{marginBottom: 50}}
-                            value={fileTest}
-                            name="fileTest"
-                            required
-                            onChange={e => setFileTest(e.target.value)}
-                        />
-                        
-                        <button className='btn btn-primary' type='submit'>Send to Classifier</button>
-                    </div>
-
-                </form>
-
+                <FileUploader/>
 
             </div>
             </div>
---
+++ Initial Build/client/src/components/FileUploader/index.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import axios from 'axios';
+
+// eslint-disable-next-line
+export const FileUploader = ({}) => {
+
+    //the reason the initial state is '[]' instead of 'null' is because its an array of files, not just one
+    const [files, setFiles] = useState([]);
+
+    //e is the parameter, in this case, e is an event
+    const onInputChange = (e) => {
+        setFiles(e.target.files);
+    }
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+
+        const data = new FormData();
+        for(let i = 0; i < files.length; i++){
+            data.append('file' + i, files[i]);
+            console.log(files[i])
+        }
+
+        axios.post('http://localhost:5000/upload', data)
+            .then( (e) => {
+                console.log('Success')
+            })
+            .catch( (e) => {
+                console.error('Error', e)
+            })
+
+    }
+
+    return (
+        <form method="post" action="#" id="#" onSubmit={submitHandler}>
+
+            <div class="max-w-md">
+                <h1 class="text-5xl font-bold">Enter Your Files Here</h1>
+                <p class="py-6">Please provide the combined JLM, perf, and test CSV files to the dropbox. 
+                These files will be sent to the Classifier in order to retrieve the classification data.</p>
+                <p style={{color:'orange', fontWeight:'bold'}}>Please ensure the files are named correctly!</p>
+                <p>Your combined JLM CSV should be titled '<b style={{color:'orange'}}>jlm.csv</b>', your combined perf CSV 
+                should be titled '<b style={{color:'orange'}}>perf.csv</b>', and your combined test CSV should be titled 
+                '<b style={{color:'orange'}}>test.csv</b>'</p>
+                
+                <label class="label">
+                    <span class="label-text" style={{marginTop: 50}}>Insert All CSV Files Here</span>
+                </label>
+                <input type="file" 
+                    class="file-input file-input-bordered file-input-accent w-full max-w-xs" 
+                    required
+                    multiple
+                    onChange={onInputChange}
+                />
+                
+                <button className='btn btn-primary' type='submit' style={{marginTop: 50}}>Send to Classifier</button>
+            </div>
+
+        </form>
+    )
+};
