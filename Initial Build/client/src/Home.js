8cd19362841911ec0b4704a8d2a8127cc2b26144
airwick989 <73313597+airwick989@users.noreply.github.com>
2022-11-06 21:11:36 -0500

    Inserted form and sample file inputs for inputting csv files to classifier

--- Initial Build/client/src/Home.js
+++ Initial Build/client/src/Home.js
@@ -1,18 +1,62 @@
+// eslint-disable-next-line
 import { Link } from 'react-router-dom';
+import React, { useState } from "react";
 
 const Home = () => {
+
+    const [file1, setFile1] = useState("");
+    const [file2, setFile2] = useState("");
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const dataSent = { file1, file2 };
+        console.log(dataSent)
+    }
+
+
     return ( 
         <div className="Home">
 
             <div class="hero min-h-screen bg-base-200">
             <div class="hero-content text-center">
-                <div class="max-w-md">
-                <h1 class="text-5xl font-bold">Hello there</h1>
-                <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                <Link to="/classifier">
-                    <button className='btn btn-primary'>Go to Classifier</button>
-                </Link>
-                </div>
+
+                <form onSubmit={handleSubmit}>
+
+                    <div class="max-w-md">
+                        <h1 class="text-5xl font-bold">Enter Your Files Here</h1>
+                        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                        
+                        <label class="label">
+                            <span class="label-text">File 1</span>
+                        </label>
+                        <input type="file" 
+                            class="file-input file-input-bordered file-input-accent w-full max-w-xs" 
+                            style={{marginBottom: 50}}
+                            value={file1}
+                            name="file1"
+                            required
+                            onChange={e => setFile1(e.target.value)}
+                        />
+
+                        <label class="label">
+                            <span class="label-text">File 2</span>
+                        </label>
+                        <input type="file" 
+                            class="file-input file-input-bordered file-input-accent w-full max-w-xs" 
+                            style={{marginBottom: 50}}
+                            value={file2}
+                            name="file2"
+                            required
+                            onChange={e => setFile2(e.target.value)}
+                        />
+                        
+                        <button className='btn btn-primary' type='submit'>Send to Classifier</button>
+                    </div>
+
+                </form>
+
+
             </div>
             </div>
 
