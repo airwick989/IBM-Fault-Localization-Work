8477f580d7762a4e1229dd2bd725ca089839f1c1
airwick989 <73313597+airwick989@users.noreply.github.com>
2022-11-21 23:58:02 -0500

    changed running port for lcClassifier server

--- Initial Build/lcClassifier/flask-server/server.py
+++ Initial Build/lcClassifier/flask-server/server.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from pickle import TRUE
+from flask import Flask, request
+from flask_cors import CORS;
+import json
+from werkzeug.utils import secure_filename
+import os
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=8000, debug=True)
+    app.run(port=5001, debug=True)
