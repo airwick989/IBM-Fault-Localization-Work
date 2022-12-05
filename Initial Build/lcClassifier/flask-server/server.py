6e6dfc1336c411fc245f7c842da139d66c98fb04
airwick989 <73313597+airwick989@users.noreply.github.com>
2022-12-04 22:50:30 -0500

    finished majority of classifier component

--- Initial Build/lcClassifier/flask-server/server.py
+++ Initial Build/lcClassifier/flask-server/server.py
@@ -16,6 +16,7 @@ from sklearn.metrics import silhouette_score
 from sklearn.model_selection import train_test_split
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import LabelEncoder
 
 app = Flask(__name__)
 CORS(app)
@@ -104,12 +105,44 @@ def processData():
     DF1 = DF1[['%MISS', 'GETS', 'SLOW', 'NONREC', 'REC', 'TIER2', 'TIER3', '%UTIL', 'AVER_HTM', 'PATTERN-NAME', 'PATTERN-NO', '_raw_spin_lock', 'ctx_sched_in', 'delay_mwaitx', 'THREADS', 'SLEEP', 'SPIN_COUNT']]
     DF1 = DF1.rename(columns = {'_raw_spin_lock' : 'RAW_SPIN_LOCK', 'ctx_sched_in' : 'CTX_SWITCH', 'delay_mwaitx' : 'DELAY_MWAITX'})
     DF_train = DF1[['GETS', 'SPIN_COUNT', 'NONREC', '%UTIL', 'AVER_HTM', 'PATTERN-NO', 'PATTERN-NAME', 'RAW_SPIN_LOCK', 'CTX_SWITCH', 'DELAY_MWAITX']]
+    
+    #THIS IS ADDED BY ME
+    DF_train2 = DF_train
+    DF_train = DF_train.drop(['PATTERN-NAME'], axis=1)
+    
     cols = []
     for col in DF_train.columns:
             cols.append(col)
-
     DF_train[cols] = scale_standard(DF_train[cols])
-    DF_train.head()
+    pca_DF_train = pd.DataFrame(data = apply_pca(DF_train), columns = ['pc1', 'pc2'])
+
+    classify(pca_DF_train, DF_train2)
+
+
+def classify(pca_DF_train, DF_train):
+    # kmeans12 = KMeans(n_clusters=3, init='k-means++', max_iter=600, n_init=10)
+    # kmeans12.fit(pca_DF_train)
+
+    # Plotting the cluster centers and the data points on a 2D plane
+    # plt.figure(figsize = (6,6))
+    # plt.style.use("seaborn")
+    # plt.scatter(pca_DF_train['pc1'], pca_DF_train['pc2'], s=50, c=DF_train['PATTERN-NO'], cmap="inferno")
+    # plt.title('PCA & KMeans cluster centroids : SyncTask Example')
+    # plt.xlabel("Principal Component 1")
+    # plt.ylabel("Principal Component 2")
+    #plt.show()
+    
+    df = pd.DataFrame({'x': pca_DF_train['pc1'], 'y': pca_DF_train['pc2'], 'z': DF_train['PATTERN-NAME']})
+    
+    plt.figure(figsize = (6,6))
+    plt.style.use("seaborn")
+    sns.color_palette("Paired")
+    groups = df.groupby('z')
+
+    for name, group in groups:
+        plt.plot(group.x, group.y, marker='o', linestyle='', markersize=3, label=name)
+    plt.legend()
+    plt.show()
 
 
 if __name__ == '__main__':
