354332bc6ea2c69bdf7786c2af1d0bb59c322256
airwick989 <73313597+airwick989@users.noreply.github.com>
2023-03-15 15:58:53 -0400

    Added a timing mechanism where the server for the crLocator observes the time delta between the currently received kafka message and the previous kafka message, and if the delta is above a threshold, only then will the crLocator handle that message

--- Initial_Build/crLocator/flask-server/server.py
+++ Initial_Build/crLocator/flask-server/server.py
@@ -9,7 +9,7 @@ from threading import Thread
 
 app = Flask(__name__)
 CORS(app)
-
+app.app_context().push()    #added to mitigate "working outside of application context" error
 
 
 
@@ -62,69 +62,81 @@ producer = KafkaProducer(
 """---- LOCALIZER FUNCTIONS ------------------------------------------------------------------------------------------------------------"""
 
 def localize():
-    #Clearing out the log directory
+    #Clearing out the logs directory
     if len(os.listdir('./logs/')) == 0:
         pass
     else:    
         for file in os.listdir('./logs/'):
             os.remove(f'./logs/{file}')
 
+    #Clearing out the Files directory
+    if len(os.listdir('./Files/')) > 0:
+        for file in os.listdir('./Files/'):
+            os.remove(f'./Files/{file}') 
+
+    jarFile = fileDB.session.query(File).filter(File.filename.like('%.jar')).first()    #another option, instead of .first(), use .all()
+    print(jarFile.filename)
 
     filename = "./Files/Hot_1.jar"
     args = "4 100"
 
-    start_time = "15"
-    recording_length = "20"
+    # start_time = "15"
+    # recording_length = "20"
 
-    delay = 1
-    script_running_time = delay + int(start_time) + int(recording_length) + delay
+    # delay = 1
+    # script_running_time = delay + int(start_time) + int(recording_length) + delay
 
-    def run_rtdriver():
-        time.sleep(delay)
-        os.system(f"./run_rtdriver.sh {start_time} {recording_length}")
+    # def run_rtdriver():
+    #     time.sleep(delay)
+    #     os.system(f"./run_rtdriver.sh {start_time} {recording_length}")
 
-    def run_jlm():
-        os.system(f"./run_jlm.sh {script_running_time} {filename} {args}")
+    # def run_jlm():
+    #     os.system(f"./run_jlm.sh {script_running_time} {filename} {args}")
 
-    rtdriver = Thread(target= run_rtdriver)
-    jlm = Thread(target=run_jlm)
-    rtdriver.start()
-    jlm.start()
+    # rtdriver = Thread(target= run_rtdriver)
+    # jlm = Thread(target=run_jlm)
+    # rtdriver.start()
+    # jlm.start()
 
-    time.sleep(script_running_time)
-    rtdriver.join()
-    jlm.join()
+    # time.sleep(script_running_time)
+    # rtdriver.join()
+    # jlm.join()
 
 
-    #Returning the method causing lock contention
-    methods = []
-    r = re.compile("^log-rt")
-    log_rt_file = list(filter(r.match, os.listdir('./logs/')))[0]
-    with open(f'./logs/{log_rt_file}') as file:
-        flag = False
-        for line in file:
-            if line.split() == ['LV', 'EVENT', 'NAME']:
-                flag = True
+    # #Returning the method causing lock contention
+    # methods = []
+    # r = re.compile("^log-rt")
+    # log_rt_file = list(filter(r.match, os.listdir('./logs/')))[0]
+    # with open(f'./logs/{log_rt_file}') as file:
+    #     flag = False
+    #     for line in file:
+    #         if line.split() == ['LV', 'EVENT', 'NAME']:
+    #             flag = True
 
-            if flag:
-                if line.split()[0] == '1':
-                    methods.append(line.split()[2])
+    #         if flag:
+    #             if line.split()[0] == '1':
+    #                 methods.append(line.split()[2])
 
-    methods = list(set(methods))
-    print("\n\n")
-    methods_str = "The method(s) causing contention in your Java program are: "
-    for method in methods:
-        methods_str = methods_str + method + ", "
-    print(methods_str)
+    # methods = list(set(methods))
+    # print("\n\n")
+    # methods_str = "The method(s) causing contention in your Java program are: "
+    # for method in methods:
+    #     methods_str = methods_str + method + ", "
+    # print(methods_str)
 
 """---- LOCALIZER FUNCTIONS ------------------------------------------------------------------------------------------------------------"""
 
 
 
 
-
+time_threshold = 5  #message time delta threshold of 5 seconds
+checkpoint_time = 0
 for message in consumer:
-    data = message.value
-    if 'signal' in data:
-        if data['signal'] == 'startLocalizer':
-            print("hello there")
+    if time.time() - checkpoint_time > time_threshold:
+        data = message.value
+        if 'signal' in data:
+            if data['signal'] == 'startLocalizer':
+                localize()
+        
+    checkpoint_time = time.time()
+    
