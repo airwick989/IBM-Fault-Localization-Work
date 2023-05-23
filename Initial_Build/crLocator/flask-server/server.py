0f1d05b69394ad578a92500fa396c2b32a849b03
airwick989 <ridwanhossain989@gmail.com>
2023-05-23 16:18:14 -0400

    Implemented localization functionality in the form of a system module

--- Initial_Build/crLocator/flask-server/server.py
+++ Initial_Build/crLocator/flask-server/server.py
@@ -62,63 +62,62 @@ def localize():
 
     #"Pull" Jar file from jarFile directory
     jarSaveDirectory = '../../jarFile/'
-    #Clearing out the jarFile directory
-    if len(os.listdir(jarSaveDirectory)) > 0:
-        for file in os.listdir(jarSaveDirectory):
-            os.remove(f'./Files/{file}') 
-    
-
-    # args = "4 100"
-
-    # start_time = "15"
-    # recording_length = "20"
-
-    # delay = 1
-    # script_running_time = delay + int(start_time) + int(recording_length) + delay
-
-    # def run_rtdriver():
-    #     time.sleep(delay)
-    #     os.system(f"./run_rtdriver.sh {start_time} {recording_length}")
-
-    # def run_jlm():
-    #     os.system(f"./run_jlm.sh {script_running_time} {filename} {args}")
-
-    # rtdriver = Thread(target= run_rtdriver)
-    # jlm = Thread(target=run_jlm)
-    # rtdriver.start()
-    # jlm.start()
-
-    # time.sleep(script_running_time)
-    # rtdriver.join()
-    # jlm.join()
+    filename = ""
+    for file in os.listdir(jarSaveDirectory):
+        filename = f"{jarSaveDirectory}{file}"
 
 
-    # #Returning the method causing lock contention
-    # methods = []
-    # r = re.compile("^log-rt")
-    # log_rt_file = list(filter(r.match, os.listdir('./logs/')))[0]
-    # prevLine = ""
-    # with open(f'./logs/{log_rt_file}') as file:
-    #     flag = False
-    #     for line in file:
-    #         if line.split() == ['LV', 'EVENT', 'NAME']:
-    #             flag = True
+    args = "4 100"
 
-    #         if flag:
-    #             if line.split()[1] not in ['0', 'EVENT']:
-    #                 # print(line.split())
-    #                 # print(prevLine)
-    #                 # print(prevLine.split())
-    #                 methods.append(prevLine.split()[2])
+    start_time = "15"
+    recording_length = "20"
 
-    #         prevLine = line
+    delay = 1
+    script_running_time = delay + int(start_time) + int(recording_length) + delay
 
-    # methods = list(set(methods))
-    # print("\n\n")
-    # methods_str = "The method(s) causing contention in your Java program are: "
-    # for method in methods:
-    #     methods_str = methods_str + method + ", "
-    # print(methods_str)
+    def run_rtdriver():
+        time.sleep(delay)
+        os.system(f"./run_rtdriver.sh {start_time} {recording_length}")
+
+    def run_jlm():
+        os.system(f"./run_jlm.sh {script_running_time} {filename} {args}")
+
+    rtdriver = Thread(target= run_rtdriver)
+    jlm = Thread(target=run_jlm)
+    rtdriver.start()
+    jlm.start()
+
+    time.sleep(script_running_time)
+    rtdriver.join()
+    jlm.join()
+
+
+    #Returning the method causing lock contention
+    methods = []
+    r = re.compile("^log-rt")
+    log_rt_file = list(filter(r.match, os.listdir('./logs/')))[0]
+    prevLine = ""
+    with open(f'./logs/{log_rt_file}') as file:
+        flag = False
+        for line in file:
+            if line.split() == ['LV', 'EVENT', 'NAME']:
+                flag = True
+
+            if flag:
+                if line.split()[1] not in ['0', 'EVENT']:
+                    # print(line.split())
+                    # print(prevLine)
+                    # print(prevLine.split())
+                    methods.append(prevLine.split()[2])
+
+            prevLine = line
+
+    methods = list(set(methods))
+    print("\n\n")
+    methods_str = "The method(s) causing contention in your Java program are: "
+    for method in methods:
+        methods_str = methods_str + method + ", "
+    print(methods_str)
 
 """---- LOCALIZER FUNCTIONS ------------------------------------------------------------------------------------------------------------"""
 
